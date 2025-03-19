abc3345b45494bfd9edcda4153ca247f594f5e98
Athan <jonpuray@gmail.com>
2025-03-20 01:25:27 +0800

    used cloudscraper

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import os
+import random
 import streamlit as st
 from playwright.sync_api import sync_playwright
+
+userAgents = [
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0',
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36	'
+    ]
 
 # Ensure Playwright browsers are installed
 PLAYWRIGHT_DIR = os.path.expanduser("~/.cache/ms-playwright")
@@ -15,7 +26,7 @@ def get_links(url):
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)  
         context = browser.new_context(
-            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+            user_agent=random.choice(userAgents)
         )
         page = context.new_page()
         page.goto(url, timeout=60000)  
