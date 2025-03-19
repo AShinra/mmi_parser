d53ea4acce87564cd0f150a7bf6211ec934ed60b
Athan <jonpuray@gmail.com>
2025-03-20 01:30:32 +0800

    used cloudscraper

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import random
 import streamlit as st
 from playwright.sync_api import sync_playwright
 
+os.system("pip install playwright")
+
 userAgents = [
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36',
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
