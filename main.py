fb062dd7432c2a89ca7561c9c6f5668b9a907957
Athan <jonpuray@gmail.com>
2025-03-23 20:40:00 +0800

    updated

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from playwright.sync_api import sync_playwright
+import streamlit as st
+
+url = st.text_input('Enter URL')
 
 pw = sync_playwright().start()
 
 browser = pw.chromium.launch()
 
 page = browser.new_page()
-page.goto('https://businessmirror.com.ph/')
+page.goto(url)
 
 page.screenshot(path='sample.png')
