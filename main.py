79e9f65deba8a9957860b7b525fe9c783e70d1e1
Athan <jonpuray@gmail.com>
2025-03-23 21:18:42 +0800

    updated

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def main():
             with st.spinner('Running App'):
                 page_content = get_page_content(url)
                 with st.expander('Test'):
-                    st.write(page_content[:100])
+                    st.write(page_content[:1000])
 
 if __name__ == '__main__':
     main()
