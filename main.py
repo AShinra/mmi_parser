134b23d93a076e381fdfc7fb6bef88a7febd778f
Athan <jonpuray@gmail.com>
2025-03-24 21:51:10 +0800

    updated

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from playwright.sync_api import sync_playwright
 import streamlit as st
 from streamlit_option_menu import option_menu
 from fetchers.fetcher_daily_tribune import dt_fetcher
-from streamlit_datetime_picker import date_time_picker, date_range_picker
 
 # Ensure Playwright browsers are installed
 os.system("playwright install chromium")
