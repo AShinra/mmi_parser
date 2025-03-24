629964c1dea9c8b7a13b0df605b7a2c2b551fe8f
Athan <jonpuray@gmail.com>
2025-03-24 14:18:28 +0800

    testing

--- fetchers/fetcher_daily_tribune.py
+++ fetchers/fetcher_daily_tribune.py
@@ -10,7 +10,7 @@ def dt_fetcher():
 
     # get section links from json file
     with open('fetchers/sections_daily_tribune.json') as json_file:
-        sections = json.load(json_file)
+        secs = json.load(json_file)
         
     pw = sync_playwright().start()
 
@@ -21,14 +21,15 @@ def dt_fetcher():
     page = browser.new_page()
     
     # go to the sections
-    for section in sections:
-        st.write(section)
-        page.goto(section)
+    for pub, sections in secs.items():
+        for section in sections:
+            st.write(section)
+            page.goto(section)
 
-        links_container = page.wait_for_selector('div#container')
-        links = links_container.query_selector_all('a')
-        for link in links:
-            st.write(link.get_attribute('href'))
+            links_container = page.wait_for_selector('div#container')
+            links = links_container.query_selector_all('a')
+            for link in links:
+                st.write(link.get_attribute('href'))
         
 
     # menu = page.wait_for_selector('#footer-menu')
