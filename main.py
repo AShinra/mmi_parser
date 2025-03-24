779b3b904e45f1606cddfcb0b6a9cad80e15e01a
Athan <jonpuray@gmail.com>
2025-03-24 21:59:57 +0800

    updated

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from playwright.sync_api import sync_playwright
 import streamlit as st
 from streamlit_option_menu import option_menu
 from fetchers.fetcher_daily_tribune import dt_fetcher
+import streamlit_shadcn_ui as ui
 
 # Ensure Playwright browsers are installed
 os.system("playwright install chromium")
@@ -18,6 +19,7 @@ if __name__ == '__main__':
     # if selected_publication == 'Daily Tribune':
     #     dt_fetcher()
 
+    my_range = ui.date_picker('DATE RANGE', mode='range', key='my_range', default_value=None)
 
     pub_name = st.selectbox(
         label='Publication',
